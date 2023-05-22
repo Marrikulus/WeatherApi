3be9c849c4862ce1805dbc4c34fb76b415f7d6ca
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2023-05-22 15:12:24 +0000

    I'm keeping the handy redirect, for easier testing

--- WeatherApi/WeatherApi/Program.cs
+++ WeatherApi/WeatherApi/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using WeatherApi;
 using WeatherApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<CityService>();
 var app = builder.Build();
-app.MapGet("/", () =>  Results.Redirect("/weather/Reykjavik"));
+app.MapGet("/", () =>  Results.Redirect("/weather/Reykjavik/history/1989-10-09"));
 app.MapControllers();
 app.Run();
